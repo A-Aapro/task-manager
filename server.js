3be8deb3c703ad11c89f62dc382d2120204a49b3
A-Aapro <61653060+A-Aapro@users.noreply.github.com>
2022-08-02 19:50:35 +0300

    express fix

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+
+const publicDirPath = path.join(__dirname, "../public/");
+app.use(express.static(publicDirPath));
 const port = process.env.PORT || 3000;
 app.listen(port);
